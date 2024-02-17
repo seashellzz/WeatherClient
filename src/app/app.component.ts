952c03139d6f2e30ed45c9b4bcf573a43f82c7eb
Seashellzz <xzzhannah@gmail.com>
2024-02-16 19:20:45 -0800

    new repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloComponent } from "./hello/hello.component";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HelloComponent, HttpClientModule]
 })
 export class AppComponent {
+
   title = 'WeatherClient';
+  
 }
