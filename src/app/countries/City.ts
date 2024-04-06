0706907f1959f6e6df3e5af548a0435a5dbdf2a3
Seashellzz <xzzhannah@gmail.com>
2024-04-05 20:40:21 -0700

    create country-cities

---
+++ src/app/countries/City.ts
@@ -0,0 +1,6 @@
+export interface City {
+    countryId: number;
+    name:string;
+    iso2:string;
+    iso3:string;
+}
---
+++ src/app/countries/country-cities.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { City } from './City';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+
+
+@Component({
+  selector: 'app-country-cities',
+  standalone: true,
+  imports: [MatTableModule],
+  templateUrl: './country-cities.component.html',
+  styleUrl: './country-cities.component.css'
+})
+export class CountryCitiesComponent {
+  public cities: City[] = [];
+  public displayedColumns : string[] = ["cityId", "name", "latitude", "longitude"];
+  id: number;
+  //public baseUrl:string = "http://localhost:5291/";
+
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
+    this.id = -1;
+  }
+
+  ngOnInit() {
+    this.getCities();
+  }
+
+  getCountries() {
+    let idparameter = this.activatedRoute.snapshot.paramMap.get("id");
+    this.id = idparameter ? +idparameter : -1;
+    this.http.get<City[]>(`${environment.baseUrl}` + `api/countries/countryCities/${this.id}`).subscribe(
+       {
+        next: result => this.cities = result,
+        error: error => console.error(error)
+      }
+      
+    );
+  }
+  
+}
