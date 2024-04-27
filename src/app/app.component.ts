6503b55f41f019dfb7655e255f9bdb232485785f
Seashellzz <xzzhannah@gmail.com>
2024-04-26 20:39:15 -0700

    authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { NavBarComponent } from "./nav-bar/nav-bar.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HelloComponent, HttpClientModule, NavBarComponent]
+    imports: [RouterOutlet, HelloComponent, NavBarComponent]
 })
 export class AppComponent {
 
--- src/app/auth/login.component.ts
+++ src/app/auth/login.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from './auth.service';
+import { LoginRequest } from './login-request';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +13,13 @@ import { RouterLink } from '@angular/router';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
-    RouterLink
+    RouterLink,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent implements OnInit{
+constructor(private authService: AuthService, private router: Router){}
   ngOnInit(): void {
     this.form = new FormGroup({
       userName: new FormControl("", Validators.required),
@@ -25,7 +28,21 @@ export class LoginComponent implements OnInit{
   }
   form!: UntypedFormGroup; 
   onSubmit(){
-    
+    let loginRequest: LoginRequest = <LoginRequest>{
+      userName :this.form.controls["username"].value,
+      password :this.form.controls["password"].value
+    };
+    this.authService.login(loginRequest).subscribe(
+      {
+        next: result => {
+          console.log(result.message);
+          this.router.navigate(["/"]);
+          
+        },
+          
+        error: error => console.error(error)
+      }
+    )
   }
   
 }
